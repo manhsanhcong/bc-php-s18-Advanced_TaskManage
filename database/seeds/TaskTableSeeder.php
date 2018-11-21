f26f6cab0729e9ba1c6a4f888361d99c9d37a7b9
manhsanhcong <manhsanhcong.08@gmail.com>
2018-11-21 14:50:36 +0700

    add file

---
+++ database/seeds/TaskTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+use Illuminate\Database\Seeder;
+use App\Task;
+class TasksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $task = new Task();
+        $task->title            = "Công việc 1";
+        $task->content          = "Nội dung công việc 1";
+        $task->image            = "";
+        $task->due_date         = "2018-09-15";
+        $task->save();
+        $task = new Task();
+        $task->title            = "Công việc 2";
+        $task->content          = "Nội dung công việc 2";
+        $task->image            = "";
+        $task->due_date         = "2018-09-16";
+        $task->save();
+        $task = new Task();
+        $task->title            = "Công việc 3";
+        $task->content          = "Nội dung công việc 3";
+        $task->image            = "";
+        $task->due_date         = "2018-09-17";
+        $task->save();
+    }
+}
+
